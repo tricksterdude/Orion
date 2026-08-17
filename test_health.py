c2f078d9cf75d8f55da074c8b6ac02fde482a62c
Richard Nelson <indecongo@gmail.com>
2026-08-17 11:23:58 +0100

    Update service host ports

--- test_health.py
+++ test_health.py
@@ -2,7 +2,7 @@ from app.health_manager import HealthManager
 
 health = HealthManager()
 
-print(health.check("http://localhost:8080"))
+print(health.check("http://localhost:8500"))
 print(health.check("http://localhost:5076"))
-print(health.check("http://localhost:3232"))
-print(health.check("http://localhost:3000"))
+print(health.check("http://localhost:3600"))
+print(health.check("http://localhost:3500"))
